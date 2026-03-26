be3304c21e1bb387eaf34cd9995c3d2cd0181f51
Rubel <mr.adcadre41@gmail.com>
2026-03-26 15:27:48 +0600

    promise.all function added

--- alltype-catch.js
+++ alltype-catch.js
@@ -5,7 +5,52 @@ function normalBtn() {
     .catch((error) => console.error("Error ", error));
 }
 
-function arrowBtn() {}
-function asyncAwaitBtn() {}
-function tryCatchFinalBtn() {}
-function PromiseAllBtn() {}
+let arrowBtn = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+};
+
+async function asyncAwaitBtn() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+}
+
+const tryCatchFinalBtn = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  try {
+    const data = await res.json();
+
+    console.log(" tried data are ", data);
+  } catch (error) {
+    console.log("Error", error);
+  } finally {
+    console.log("Nothing is impossible finally");
+  }
+};
+
+async function PromiseAllBtn() {
+  try {
+    const [users, posts, comments] = await Promise.all([
+      fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+        res.json(),
+      ),
+
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+        res.json(),
+      ),
+
+      fetch("https://jsonplaceholder.typicode.com/comments").then((res) =>
+        res.json(),
+      ),
+    ]);
+
+    console.log(users);
+    console.log(posts);
+    console.log(comments);
+  } catch (error) {
+    console.log(error);
+  }
+}
